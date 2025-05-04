d6d9d959235a4939dae47ef1ea53171405724bfc
Iyedchaabane <chaabaneiyed@gmail.com>
2025-05-05 00:44:56 +0100

    add env in nestjs.yml file

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -14,7 +14,7 @@ function Education() {
   return (
     <div id="education" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <Image
-        src="${basePath}/section.svg"
+        src={`${basePath}/section.svg`}
         alt="Hero"
         width={1572}
         height={795}
