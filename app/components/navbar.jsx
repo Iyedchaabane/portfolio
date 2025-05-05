4ed4a4a3b051e477b595911db95fab1b815f6b6d
Iyed <93859455+Iyedchaabane@users.noreply.github.com>
2025-05-05 10:26:39 +0100

    Fix: Use dynamic basePath for internal hash links

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,6 +1,6 @@
 // @flow strict
 import Link from "next/link";
-
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 function Navbar() {
   return (
@@ -8,32 +8,29 @@ function Navbar() {
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
           <Link
-            href="/"
+            href={`${basePath}/`}
             className=" text-[#16f2b3] text-3xl font-bold">
-            IYED CH
+            IYED CHAABANE
           </Link>
         </div>
 
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#about`}>
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#experience`}><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#skills`}><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#education`}><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
           </li>
-          {/*<li>*/}
-          {/*  <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link>*/}
-          {/*</li>*/}
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href={`${basePath}/#projects`}><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
           </li>
         </ul>
       </div>
